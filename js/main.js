8ffa658a4ef3ad00bf13ce9f1d079c53713098ee
Ivan Jovkovic <jokac14@gmail.com>
2018-09-21 14:53:04 +0200

    sticky-bar, houses gallery

--- js/main.js
+++ js/main.js
@@ -135,15 +135,22 @@ $("#slideshow > div:gt(0)").hide();
 
 setInterval(function () {
 	$('#slideshow > div:first')
-		.fadeOut(3000)
+		.fadeOut(2000)
 		.next()
-		.fadeIn(3000)
+		.fadeIn(2000)
 		.end()
 		.appendTo('#slideshow');
-}, 9000);
+}, 6000);
 
 $(document).ready(function () {
 	$(".login").click(function () {
 		$("#login-div").toggle();
+	});
+});
+
+
+$(document).ready(function () {
+	$(".social").hover(function () {
+		$(this).find("span").toggleClass("active");
 	});
 });
